80ffa2d0e37d00cdbdf5e8973677ffb123b5c4a6
Mikael <13247629622@163.com>
2022-01-28 18:09:18 +0800

    优化model

--- order-api/order.go
+++ order-api/order.go
@@ -15,7 +15,7 @@ import (
 	"github.com/tal-tech/go-zero/rest"
 )
 
-var configFile = flag.String("f", "/Users/mikael/Developer/goenv/gozerodtm/order-api/etc/order.yaml", "the config file")
+var configFile = flag.String("f", "etc/order.yaml", "the config file")
 
 func main() {
 
